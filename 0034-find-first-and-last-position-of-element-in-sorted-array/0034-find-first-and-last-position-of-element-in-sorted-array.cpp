4784b857db481134307947187e0daf92de1c716e
suhani-1203 <sagarwal1_be22@thapar.edu>
2024-10-12 11:48:04 +0530

    Time: 3 ms (89.49%), Space: 16.4 MB (40.98%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,83 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans;
-        int first=-1,last=-1,low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+((high-low)/2);
-            if(nums[mid]==target){
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target)
-                low=mid+1;
-            else
-                high=mid-1; 
-        }
-        if(first==-1){
-            ans.push_back(first);
-            ans.push_back(last);
-            return ans;
-        }
-        else{
-            low=0;high=nums.size()-1;
-            while(low<=high){
-                int mid=low+((high-low)/2);
-                if(nums[mid]<=target){
-                    last=mid;
-                    low=mid+1;
-                }
-                else
-                    high=mid-1;
+    /*int lowerboundOptimal(vector<int>& arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        ans.push_back(first);
-        ans.push_back(last);
         return ans;
+    }
+
+    int upperboundOptimal(vector<int>& arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int n = arr.size();
+        int lb = lowerboundOptimal(arr, n, target);
+        if (lb == n || arr[lb] != target) {
+            return {-1, -1};
+        }
+        int ub = upperboundOptimal(arr, n, target);
+        return {lb, ub - 1};
+    }
+};*/
+
+    int lowerBoundOptimal(vector<int>& arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans=n;
+        while (low <= high) {
+            int mid = (low +high)/2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+    int upperBoundOptimal(vector<int>& arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans=n;
+        while (low <= high) {
+            int mid = (low+high)/2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int n = arr.size();
+        int lb = lowerBoundOptimal(arr, n, target);
+        if ((lb == n) || (arr[lb] != target))
+            return {-1, -1};
+        else {
+            int ub = upperBoundOptimal(arr, n, target);
+            return {lb, ub - 1};
+        }
     }
 };

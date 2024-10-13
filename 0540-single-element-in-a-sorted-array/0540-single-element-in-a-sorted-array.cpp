ddc0123b9a344b5a8b0c7472462c5c05ba828808
suhani-1203 <sagarwal1_be22@thapar.edu>
2024-10-13 10:58:19 +0530

    Time: 12 ms (93.98%), Space: 25.1 MB (13.13%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -14,18 +14,10 @@ public:
             int mid=low+(high-low)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
                 return nums[mid];
-            else if(mid%2==0){
-                if(nums[mid+1]!=nums[mid])
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }
-            else{
-                if(nums[mid-1]==nums[mid])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }
+            else if((mid%2==0 && nums[mid]==nums[mid+1])||(mid%2==1 && nums[mid]==nums[mid-1]))
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
     }

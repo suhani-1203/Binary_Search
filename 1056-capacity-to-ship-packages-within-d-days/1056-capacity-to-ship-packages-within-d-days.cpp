6cde39435a0aef31512d68c67aed2b622f85fe25
suhani-1203 <sagarwal1_be22@thapar.edu>
2024-10-15 09:06:16 +0530

    Time: 50 ms (5.18%), Space: 33.6 MB (92.52%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool possible(vector<int>&arr,int totalDays,int capacity){
+        int totalWeight=0,noOfDays=1;
+        for(int i=0;i<arr.size();i++){
+            if((totalWeight+arr[i])>capacity){
+                noOfDays++;
+                totalWeight=arr[i];
+                if(noOfDays>totalDays)
+                    return false;
+            }
+            else{
+                    totalWeight+=arr[i];
+            }
+        }
+        return noOfDays<=totalDays;
+
+    } 
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
+        int ans;
+        int maxWeight=0,minWeight=INT_MIN;
+        for(int i=0;i<n;i++){
+            maxWeight+=weights[i];
+            minWeight=max(minWeight,weights[i]);
+        }
+        int low=minWeight,high=maxWeight;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(weights,days,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
